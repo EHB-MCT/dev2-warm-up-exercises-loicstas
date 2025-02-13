97a8424ee3379404e7745564e4137e22da056deb
Loic Stas <loic.stas@student.ehb.be>
2025-02-13 16:01:36 +0100

    warm

--- main.js
+++ main.js
@@ -8,7 +8,13 @@ runTitle();
 
 function warmup1() {
     console.log("Exercise week 1");
+    const button = document.querySelector('#button-1a')
+    button.addEventListener('click', function () {
+        console.log("button ok ");
+});
+    document.querySelector('#content-1').innerHTML = '<p>hello world</p>'
 }
+
 
 function warmup2() {
     console.log("Exercise week 2");
