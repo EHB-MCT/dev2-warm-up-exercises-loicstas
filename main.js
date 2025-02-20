a2c3c5d929ba45a36443228c9fa158032a0ee617
Loic Stas <loic.stas@student.ehb.be>
2025-02-20 16:11:42 +0100

    logo

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import {
 } from './lib.js';
 
 runTitle();
+warmup1(); 
+warmup2(); 
 
 function warmup1() {
     console.log("Exercise week 1");
@@ -19,7 +21,29 @@ function warmup1() {
 function warmup2() {
     console.log("Exercise week 2");
     const scores = getRandomScores(10);
+    //get ther button
+    const button = document.querySelector('#button-2a')
+    button.addEventListener('click', function () {
+    console.log("button ok ");
+        
+        let html = '<ul>'; 
+        scores.forEach (function (score) {     
+        console.log(scores);
+        console.log (score);
+        html += <li>(score)</li>
+    })
+        
+
+    }); 
+    html += '</ul>'; 
+
+const html='<ul><li>ok</li></ul>'
+document.querySelector('#content-2').innerHTML = html;
 }
+
+
+
+
 
 function warmup3() {
     let student = getStudentJSON();
